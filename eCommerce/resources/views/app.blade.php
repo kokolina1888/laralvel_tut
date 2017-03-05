a29e3a7317dc8f6eeb3f3aa096cd080a6ae8797f
Milena Tomova <kokolina1888@gmail.com>
2017-03-05 08:31:06 +0200

    new project

---
+++ eCommerce/resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Welcome to We Dew Lawns</title>
+</head>
+<body>
+	<h1>Welcome to professional Lawn Care Service Provider</h1>
+	@yield('content')
+</body>
+</html>
