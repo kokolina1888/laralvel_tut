15b6266cbd0bd346fa2342f7f7bec0713a967074
Milena Tomova <kokolina1888@gmail.com>
2017-05-19 22:06:38 +0300

    up to 42

--- corporate/app/Http/Controllers/Admin/MenusController.php
+++ corporate/app/Http/Controllers/Admin/MenusController.php
@@ -15,9 +15,11 @@ use Gate;
 use Menu;
 use Auth;
 
+use Corp\Category;
+
 class MenusController extends AdminController
 {
-    
+
     protected $m_rep;
     
     
@@ -51,43 +53,43 @@ class MenusController extends AdminController
     public function index()
     {
         //
-        
+
         $menu = $this->getMenus();
      $this->title = 'Menu Menager';   // dd($menu);
-        
-        $this->content = view(env('THEME').'.admin.menus_content')->with('menus',$menu)->render();
-        
-        return $this->renderOutput();
-    }
-    
-    public function getMenus()
-    {
+
+     $this->content = view(env('THEME').'.admin.menus_content')->with('menus',$menu)->render();
+
+     return $this->renderOutput();
+ }
+
+ public function getMenus()
+ {
         //
-        
-        $menu = $this->m_rep->get();
-        
-        if($menu->isEmpty()) {
-            return FALSE;
-        }
-        
-        return Menu::make('forMenuPart', function($m) use($menu) {
-            
-            foreach($menu as $item) {
-                if($item->parent == 0) {
-                    $m->add($item->title,$item->path)->id($item->id);
-                }
-                
-                else {
-                    if($m->find($item->parent)) {
-                        $m->find($item->parent)->add($item->title,$item->path)->id($item->id);
-                    }
+
+    $menu = $this->m_rep->get();
+
+    if($menu->isEmpty()) {
+        return FALSE;
+    }
+
+    return Menu::make('forMenuPart', function($m) use($menu) {
+
+        foreach($menu as $item) {
+            if($item->parent == 0) {
+                $m->add($item->title,$item->path)->id($item->id);
+            }
+
+            else {
+                if($m->find($item->parent)) {
+                    $m->find($item->parent)->add($item->title,$item->path)->id($item->id);
                 }
             }
-            
-        });
-        
+        }
 
-    }
+    });
+
+
+}
 
     /**
      * Show the form for creating a new resource.
@@ -96,8 +98,53 @@ class MenusController extends AdminController
      */
     public function create()
     {
-        //
+     $this->title = 'New menu Item';
+     $tmp = $this->getMenus()->roots();
+
+     $menus = $tmp->reduce(function($returnMenus, $menu){
+        $returnMenus[$menu->id] = $menu->title;
+        return $returnMenus;
+    }, ['0'=>'parent item']);
+
+     $categories = Category::select('*')->get();
+
+     $list = [];
+     $list = array_add($list, 0, 'not in use');
+     $list = array_add($list, 'parent', 'Blog');
+
+     foreach($categories as $category){
+        if($category->parent_id == 0){
+            $list[$category->title] = [];
+        } else {
+           
+            $list[$category->where('id', $category->parent_id)->first()->title][] = $category->title;
+        }
     }
+
+   $articles = $this->a_rep->get('*');
+
+    $articles = $articles->reduce(function($returnArticles, $article){
+        $returnArticles[$article->alias] = $article->title;
+        return $returnArticles;
+    }, []);
+
+   $filters = \Corp\Filter::select('id','title','alias')->get()->reduce(function ($returnFilters, $filter) {
+            $returnFilters[$filter->alias] = $filter->title;
+            return $returnFilters;
+        }, ['parent' => 'Раздел портфолио']);
+        
+        $portfolios = $this->p_rep->get(['id','alias','title'])->reduce(function ($returnPortfolios, $portfolio) {
+            $returnPortfolios[$portfolio->alias] = $portfolio->title;
+            return $returnPortfolios;
+        }, []);
+        
+        $this->content = view(env('THEME').'.admin.menus_create_content')->with(['menus'=>$menus,'categories'=>$list,'articles'=>$articles,'filters' => $filters,'portfolios' => $portfolios])->render();
+        
+        
+        
+        return $this->renderOutput();
+        
+}
 
     /**
      * Store a newly created resource in storage.
